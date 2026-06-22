5df40b8e39e75aa45745c53ea48d19ccd3762e9a
RaghavendraBarc7 <paritiraghav2@gmail.com>
2026-06-21 20:14:09 -0400

    Solve numpy-row-extremes

---
+++ study-plans/numpy-basics/numpy-row-extremes/numpy-row-extremes.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def row_extremes(data):
+    """Returns: np.ndarray of shape (4, m), rows are max_val, max_col, min_val, min_col"""
+    data = np.asarray(data, dtype = np.float64)
+    return np.stack([np.max(data, axis = 1), np.argmax(data, axis = 1), np.min(data, axis = 1), 
+          np.argmin(data, axis = 1)])
