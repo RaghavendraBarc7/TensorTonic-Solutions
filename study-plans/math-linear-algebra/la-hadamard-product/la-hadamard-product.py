bd315e673b5cfc2ecfddc1d7732c5efa3ba76163
RaghavendraBarc7 <paritiraghav2@gmail.com>
2026-09-10 22:42:15 -0400

    Solve la-hadamard-product

---
+++ study-plans/math-linear-algebra/la-hadamard-product/la-hadamard-product.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def hadamard_product(A, B):
+    """
+    Returns: ndarray, the element-wise product A * B.
+    """
+    return np.array(A) * np.array(B) # np.multiply(A, B)
+    pass
